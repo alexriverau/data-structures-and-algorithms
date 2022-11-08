b8542cf454eb000611f83aeaf84369b3db56dded
Alejandro Rivera <alexrivera78@gmail.com>
2022-11-08 01:48:49 -0600

    updates code challenge 11

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -11,5 +11,16 @@ class PseudoQueue:
         self.stack.push(value)
 
     def dequeue(self):
-        pass
+        if self.stack is None:
+            return None
+
+        while self.stack:
+            pop_stack_val = self.stack.pop()
+            push_temp_val = self.temp.push(pop_stack_val)
+
+        while self.temp:
+            pop_temp = self.temp.pop()
+            self.stack.push(pop_temp)
+
+        return push_temp_val
 
