ddacdabf18520fd7c8a10ae7047d42f33561a672
Alejandro Rivera <alexrivera78@gmail.com>
2023-01-24 00:21:31 -0600

    updates code

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -7,7 +7,7 @@ def left_join(left_hashmap, right_hashmap):
     for key in left_hashmap:
         left_value = left_hashmap[key]
 
-        for r_key in right_hashmap.items():
+        for r_key in right_hashmap:
             if key == r_key:
                 right_value = right_hashmap[r_key]
             else:
